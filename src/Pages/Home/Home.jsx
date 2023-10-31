be084bd93b6fddb3cc61f7c16a445d05ce71484d
sajjad <sajjadmoeini97@gmail.com>
2023-11-01 02:14:43 +0330

    add store

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './Home.css'
+import allInfos from './../../store'
+console.log(allInfos)
 export default function Home() {
   return (
     <>
-    <div className="home-bg-container position-absolute w-100">
-      <div className="home-bg-layout w-100 h-100"></div>
-    </div>
+      <div className="home-bg-container position-absolute w-100">
+        <div className="home-bg-layout w-100 h-100"></div>
+      </div>
+      <div className="home-title-container d-flex-centering w-100">
+        <h1 className='home-title text-light'>
+          به فوت کورت راد خوش آمدید
+        </h1>
+      </div>
     </>
   )
 }
